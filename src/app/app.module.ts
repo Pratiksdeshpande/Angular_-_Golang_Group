6a32c0a43de49e22598b02a8775a85eb78832fa7
Pratik <pdeshpande666@gmail.com>
2021-11-11 23:13:33 +0530

    Add buttons for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SignupandLoginComponent } from './signupand-login/signupand-login.component';
+import { NotepadComponent } from './notepad/notepad.component';
+import { DigitalResumeComponent } from './digital-resume/digital-resume.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { MeetRoomBookingSystemComponent } from './meet-room-booking-system/meet-room-booking-system.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupandLoginComponent,
+    NotepadComponent,
+    DigitalResumeComponent,
+    TodolistComponent,
+    MeetRoomBookingSystemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
