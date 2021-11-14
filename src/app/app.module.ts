d53eb6baf2d565a4e3573cd1825d34764c6519a2
Pratik <pdeshpande666@gmail.com>
2021-11-15 03:30:50 +0530

    pratik - 15-11-2021:03:30 add angular material to UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule,FormsModule} from "@angular/forms";
 
-import { AppRoutingModule,routingComponents } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+
+
 
 @NgModule({
   declarations: [
@@ -15,7 +22,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,MatInputModule,
+    FormsModule,MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/digital-resume/digital-resume.component.ts
+++ src/app/digital-resume/digital-resume.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormGroupDirective, Validators } from "@angular/forms";
+import {MAT_FORM_FIELD, MatFormField, MatFormFieldControl} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-digital-resume',
@@ -7,4 +8,29 @@ import { FormControl, FormGroup, FormGroupDirective, Validators } from "@angular
   styleUrls: ['./digital-resume.component.css']
 })
 export class DigitalResumeComponent  {
-}
+  
+  rform= new FormGroup({
+    mName:new FormControl('',Validators.required),
+    lName:new FormControl(''),
+    month:new FormControl('')
+    
+  })
+  post(val: any){
+    console.log(val.mName)
+  }
+  display = false;
+  generatecv(){
+    this.display = true;
+  }
+  public firstname='';
+  public middlename='';
+  public Lastname='';
+  public gender='';
+  public contactNo ='';
+  public telephoneNo ='';
+  public email ='';
+  public city ='';
+  public state ='';
+  public pincode ='';
+  
+  };
