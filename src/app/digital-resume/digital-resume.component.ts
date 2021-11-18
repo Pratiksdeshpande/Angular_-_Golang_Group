864d747c61f4b11759a0409df750edbafa2b6dd4
Pratik <pdeshpande666@gmail.com>
2021-11-19 02:46:41 +0530

    change template driven form to reactive form | Add dynamic field adding control to educational field

--- src/app/digital-resume/digital-resume.component.ts
+++ src/app/digital-resume/digital-resume.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { FormArray, FormControl, FormGroup, FormGroupDirective, FormGroupName, Validators } from "@angular/forms";
+import { FormControl, FormGroup,FormArray, FormGroupName, Validators } from "@angular/forms";
 import {MAT_FORM_FIELD, MatFormField, MatFormFieldControl} from '@angular/material/form-field';
 
 @Component({
@@ -8,6 +8,7 @@ import {MAT_FORM_FIELD, MatFormField, MatFormFieldControl} from '@angular/materi
   styleUrls: ['./digital-resume.component.css']
 })
 export class DigitalResumeComponent  {
+
   public firstname='';
   public middlename='';
   public Lastname='';
@@ -22,36 +23,45 @@ export class DigitalResumeComponent  {
   public instituteName='';
   public passingYear='';
   public percentage='';
-
-
+  public courseName ='';
 
 
   personaldata = new FormGroup({
-    'UserName': new FormControl(null),
-    'UserLastName': new FormControl(null),
+    'userAbout': new FormGroup({
+      'UserName': new FormControl(''),
+      'UserMiddleName': new FormControl(''),
+      'UserLastName': new FormControl(''),
+    }),
+    'usercontact': new FormGroup({
+      'UserPhoneNo': new FormControl(''),
+      'UsertelephoneNo': new FormControl(''),
+      'UserEmail': new FormControl(''),
+    }),
     'useraddress' : new FormGroup({
       'usercity': new FormControl(''),
       'userstate': new FormControl(''),
-      
-    })
-  })
-  onSubmit() {
-    console.log(this.personaldata);
-  }
-  
-  // arr = [null];
-  // add(){
-    //   this.arr.push(1);
-  //   console.log(this.arr);
-  // }
+      'userpincode': new FormControl(''),
+    }),
+    'userSammary': new FormControl('')
+  });
+
+  educationaldata = new FormGroup({
+    'educationalField': new FormArray([])
+  });
 
   
-  // rform= new FormGroup({
-    //   mName:new FormControl('',Validators.required),
-  //   lName:new FormControl(''),
-  //   month:new FormControl('')
-    
-  // })
+
+  get EDUcontrols(){
+    return (<FormArray>this.educationaldata.get('educationalField')).controls;
+  }
+
+  onsubmit() {
+    console.log(this.personaldata.value);
+  }
+  addEduField(){
+    const control=new FormControl('');
+    (<FormArray>this.educationaldata.get('educationalField')).push(control);
+  }
 
   display = false;
   generatecv(){
